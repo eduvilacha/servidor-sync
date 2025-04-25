167d9f6f9f2ac16e97749312f22d9e09dab0d1dc
edu.vilacha <edu.vilacha@hotmail.com>
2025-04-25 19:04:40 +0200

    rgistro error 2

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 });
 
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 // -------------------------RUTAS-------------------------------
 
